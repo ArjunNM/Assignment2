6207b93fba444d5cab114fca6f9e6eb1ab85a103
Tanvi <tanvv6600@gmail.com>
2023-02-06 13:51:15 -0500

    User Menu Creation

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Test");
+﻿/*********************************************************
+ * Created by : Arjun Nandanath Madathil , Tanvi Koduru
+ * Created on : 5th Feb 2023
+ * *******************************************************/
+
+// Variable declarations
+
+int inpVal;
+do
+{
+
+    Console.WriteLine("*******************************Menu*****************************");
+    Console.WriteLine("\n1. Get Circle Radius");
+    Console.WriteLine("\n2. Change Circle Radius");
+    Console.WriteLine("\n3. Get Circle Circumference");
+    Console.WriteLine("\n4. Get Circle Area");
+    Console.WriteLine("\n5. Exit");
+    inpVal = Convert.ToInt32(Console.readline());
+    if( inpVal < 1 || inpVal > 5 )
+    {
+        Console.WriteLine("Invalid Input");
+    }
+} while (inpVal != 5);
